c952f4c1538df7c9094af5d328c685e3bb840593
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-02-18 16:44:55 +0200

    Setup react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+
 import About from "./components/about/About";
 import Client from "./components/client/Client";
-import Contacts from "./components/contacts/Contacts";
 import Footer from "./components/footer/Footer";
-import Header from "./components/hero/Header";
-import Slider from "./components/hero/Slider";
 import Info from "./components/info/Info";
-import FeatureWatches from "./components/products/FeatureWatches";
-import NewArrivals from "./components/products/NewArrivals";
-import TopSaleWatches from "./components/products/TopSaleWatches";
-import Service from "./components/service/Service";
+import Products from "./components/products/Products";
+import Home from "./components/home/Home";
+import Header from "./components/Header/Header";
+import ContactsUs from "./components/contactsUs/ContactsUs";
 
 function App() {
   return (
     <>
-      <div className="hero_area">
-        <Header />
-        <Slider />
-      </div>
-      <Service />
-      <About />
-      <TopSaleWatches />
-      <FeatureWatches />
-      <NewArrivals />
-      <Contacts />
-      <Client />
-      <Info />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/client" element={<Client />} />
+        <Route path="/contacts-us" element={<ContactsUs />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
       <Footer />
     </>
   );
