fbab828b24b0f1445f8ee4316e746eaee0b1a365
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-03-01 17:10:13 +0200

    Add abstract requester

---
+++ client/src/api/requester.js
@@ -0,0 +1,30 @@
+export default async function requester(method, url, data) {
+  const options = {};
+
+  if (method !== "GET") {
+    options.method = method;
+  }
+
+  if (data) {
+    options.headers = {
+      ...options.headers,
+      "Content-Type": "application/json",
+    };
+
+    options.body = JSON.stringify(data);
+  }
+
+  const response = await fetch(url, data);
+  const result = await response.json();
+
+  if (!response.ok) {
+    throw result;
+  }
+
+  return result;
+}
+
+export const get = requester.bind(null, "GET");
+export const post = requester.bind(null, "POST");
+export const put = requester.bind(null, "PUT");
+export const del = requester.bind(null, "DELETE");
