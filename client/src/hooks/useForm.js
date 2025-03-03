cb0934f7675f5679641b2bcd6137dcbc4eca72fc
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-03-03 11:02:59 +0200

    Add useForm costom hook

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function useForm(initialValues, submitCallback) {
+  const [values, setValues] = useState(initialValues);
+
+  const changeHandler = (e) => {
+    setValues((pervState) => ({
+      ...pervState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault(e);
+    submitCallback(values);
+  };
+
+  return {
+    values,
+    changeHandler,
+    submitHandler,
+  };
+}
+
+export default useForm;
