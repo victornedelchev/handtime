ed48207c2b451b804bf33699de2930cc92ee7aac
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-03-24 15:52:41 +0200

    Add endpoin for get weather

---
+++ client/src/constants/weatherURL.js
@@ -0,0 +1,7 @@
+const city = "Sofia";
+
+const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+  import.meta.env.VITE_APP_ID
+}`;
+
+export default WEATHER_URL;
