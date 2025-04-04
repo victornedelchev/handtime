3454246bb731e9e45930ea4e3e4f724df786dc7e
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2025-04-04 16:38:59 +0300

    Fix spinner in the center

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,5 +1,9 @@
 import "./Loader.css";
 
 export default function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
 }
